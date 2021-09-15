5de550cb4ba1442c58354d3ba6016f47d953b2d8
MaxWesley <mx_wesley@hotmail.com>
2021-09-15 00:39:59 -0300

    refactor: Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,98 +1,27 @@
 import "./app.css";
 
-import { useState, FormEvent } from "react";
+import { FormEvent } from "react";
 
-import { BiSearchAlt } from 'react-icons/bi';
+import { SearchInput } from "./components/SearchInput";
+import { SearchButton } from "./components/SearchButton";
 
-import axios from 'axios';
-
-import Modal from 'react-modal';
-
-Modal.setAppElement('#root');
+import { SearchProvider } from './contexts/search';
+import { ModalResult } from "./components/Modal";
 
 function App() {
-  const [searchInput, setSearchInput] = useState('');
-  const [modalIsOpen, setIsOpen] = useState(false);
-
-  const [products, setProducts] = useState([]);
-
-  const openModal = () => setIsOpen(true);
-
-  const closeModal = () => setIsOpen(false);
-
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-
-    console.log()
-    openModal();
-  }
-
-  const getListProduct = async (search: string) => {
-    const baseUrl = ` https://mystique-v2-americanas.juno.b2w.io/autocomplete?content=${search}&source=nanook`;
-
-    await axios.get(baseUrl).then(response => {
-      setProducts(response.data?.products);
-    }).catch(err => {
-      console.error(err);
-    });
-  }
+  const handleSubmit = (event: FormEvent) => event.preventDefault();
 
   return (
-    <div className="App">
-      <form onSubmit={handleSubmit}>
-        <label>
-          <span>Buscar produto </span>
-          <input
-            type="text"
-            placeholder="O que você deseja?"
-            value={searchInput}
-            onChange={(text) => setSearchInput(text.target.value)}
-          />
-        </label>
-        <button className="active">
-          <BiSearchAlt size={22} />
-          <span>consultar</span>
-        </button>
-      </form>
-
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-        style={customStyles}
-        contentLabel="Example Modal"
-        onAfterOpen={() => getListProduct(searchInput)}
-      >
-        <h2>MODAL</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>#ID</th>
-              <th>Produto</th>
-            </tr>
-          </thead>
-          <tbody>
-            {products.map((product: any) => (
-              <tr>
-                <td>{product?.id}</td>
-                <td>{product?.name}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </Modal>
-    </div>
+    <SearchProvider>
+      <div className="App">
+        <form onSubmit={handleSubmit}>
+          <SearchInput />
+          <SearchButton />
+        </form>
+        <ModalResult />
+      </div>
+    </SearchProvider>
   );
 }
-
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-  },
-};
 
 export default App;
